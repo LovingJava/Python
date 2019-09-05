2c5c55bc98e7147fc118d515689af44d2c80bafd
Justin Doh <kudohsbiz@gmail.com>
2019-09-04 17:16:48 -0700

    Create ImportMod.py

---
+++ ImportMod.py
@@ -0,0 +1,12 @@
+#import ImportMod as mm
+from ImportMod import find_index, test
+#from ImportMod import *
+import sys
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+index = find_index(courses, 'Math')
+
+#print(index)
+#print(test)
+print(sys.path)
