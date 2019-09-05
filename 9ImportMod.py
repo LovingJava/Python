0ce63656c0129d2f0e85d6c9d444348fa4ba5fa9
Justin Doh <kudohsbiz@gmail.com>
2019-09-04 17:17:40 -0700

    Update and rename ImportMod.py to 9ImportMod.py

--- 9ImportMod.py
+++ 9ImportMod.py
@@ -1,3 +1,17 @@
+print('Imported my_module...')
+
+test = 'Test String'
+
+def find_index(to_search, target):
+    for i, value in enumerate(to_search):
+        if value == target:
+            return i
+
+    return -1
+   
+  
+  
+
 #import ImportMod as mm
 from ImportMod import find_index, test
 #from ImportMod import *
