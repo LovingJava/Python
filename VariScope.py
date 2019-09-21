32f21c7bebc79ff5d3fc1c5826787c02927af904
Justin Doh <kudohsbiz@gmail.com>
2019-09-21 06:09:04 -0700

    Update VariScope.py

--- VariScope.py
+++ VariScope.py
@@ -1,13 +1,28 @@
-'''
-LEGB
-Local. Enclosing, Global, Built-in
-'''
+# x = 'global x'
+
+# def test(z):
+#     global x
+#     x = 'local x'
+#     print(z)
+
+# test('local z')
+# print(x)
+
 x = 'global x'
 
-def test():
-    # global x
-    x = 'local x'
+def outer():
+    #x = 'outer x'
+
+    def inner():
+        #nonlocal x
+        #x = 'inner x'
+        print(x)
+    
+    inner()
     print(x)
 
-test()
-print(x)
+outer()
+print(x)     
+    
+
+
